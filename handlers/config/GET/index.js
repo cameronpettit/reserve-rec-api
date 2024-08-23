0c53aa47ef5f7203716bf76960d30048b9a9e434
Daniel Truong <daniel@digitalspace.ca>
2024-08-23 18:45:41 +0000

    Adding confg endpoint

---
+++ handlers/config/GET/index.js
@@ -0,0 +1,31 @@
+const { runQuery, TABLE_NAME } = require('/opt/dynamodb');
+const { sendResponse, checkWarmup, logger } = require('/opt/base');
+
+exports.handler = async (event, context) => {
+    logger.debug('Read Config', event);
+    // Allow CORS
+    if (event.httpMethod === 'OPTIONS') {
+        return sendResponse(200, {}, 'Success', null, context);
+    }
+
+    if (checkWarmup(event)) {
+        return sendResponse(200, [], 'Warm up.', null);
+    }
+
+    let queryObj = {
+        TableName: TABLE_NAME
+    };
+
+    try {
+        queryObj.ExpressionAttributeValues = {};
+        queryObj.ExpressionAttributeValues[':pk'] = { S: 'config' };
+        queryObj.ExpressionAttributeValues[':sk'] = { S: 'config' };
+        queryObj.KeyConditionExpression = 'pk =:pk AND sk =:sk';
+
+        const configData = await runQuery(queryObj);
+        return sendResponse(200, configData.items[0], 'Success', null, context);
+    } catch (err) {
+        logger.error(err);
+        return sendResponse(400, [], 'Error', err, context);
+    }
+};
---
+++ handlers/config/__tests__/get.test.js
@@ -0,0 +1,37 @@
+const { createDB, deleteDB, getHashedText } = require('../../../__tests__/settings');
+const { MockData } = require('../../../__tests__/mock_data');
+
+const data = new MockData;
+let dbClient;
+
+describe('Specific Park Names GET', () => {
+    const OLD_ENV = process.env;
+    beforeEach(async () => {
+        jest.resetModules();
+        const hash = getHashedText(expect.getState().currentTestName);
+        process.env.TABLE_NAME = hash;
+        dbClient = await createDB(data.allData(), hash);
+    });
+
+    afterEach(() => {
+        deleteDB(process.env.TABLE_NAME);
+        process.env = OLD_ENV; // Restore old environment
+    });
+
+    test('GET config', async () => {
+        const lambda = require('../GET/index');
+        const res = await lambda.handler({});
+        const warmup = await lambda.handler({ warmup: true });
+        expect(JSON.parse(res.body).data).toEqual(data.mockConfig);
+        expect(warmup.statusCode).toEqual(200);
+    });
+
+    test('GET config error', async () => {
+        // If query error
+        const dynamodbLayer = require('../../../.aws-sam/build/AWSUtilsLayer/dynamodb');
+        jest.spyOn(dynamodbLayer, 'runQuery').mockReturnValue(() => new Error);
+        const lambda = require('../GET/index');
+        const error = await lambda.handler({});
+        expect(error.statusCode).toBe(400);
+    });
+});
